b660f9a32afd1e475770ad18073c3745f4c070cc
Arver1 <aactepanov@mail.ru>
2017-11-10 02:24:04 +0300

    style mobil catalog

--- js/index_popUp.js
+++ js/index_popUp.js
@@ -1,9 +1,9 @@
 'use strict';
 (function() {
 	var btnOrder = document.querySelector('.btn.btn-order');
-	var goodsOrderWrapper = document.querySelector('.goods__order-wrapper');
+	var maskPopUp = document.querySelector('.mask-popup');
 	btnOrder.addEventListener('click', function(evt) {
 		evt.preventDefault();
-		window.util.showPopUp(goodsOrderWrapper,'visually-hidden');
+		window.util.showPopUp(maskPopUp,'visually-hidden');
 });
 })();
