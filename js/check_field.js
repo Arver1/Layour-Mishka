88f174370f1dc93a56565a6b190b3ff405159266
Arver1 <aactepanov@mail.ru>
2017-11-17 23:27:45 +0300

    mobil form compl

--- js/check_field.js
+++ js/check_field.js
@@ -1,17 +1,34 @@
 'use strict';
 (function(){
-	var formInitials = document.querySelectorAll('.form-initials__wrapper input');
-	for(var i = 0; i < formInitials.length; i++) {
-		formInitials[i].addEventListener('blur', function(evt) {
-			if(evt.target.value != '') {
-				if(!evt.target.classList.contains('form-initials__field-hide')) {
-					evt.target.classList.add(evt.target.className.split(' ')[0]+'-hide');
-				}
-			} else {
-				if(evt.target.classList.contains('form-initials__field-hide')) {
-					evt.target.classList.remove('form-initials__field-hide');
-				}
-			}
+	var classNameHide;
+	var formInitField = document.querySelectorAll('.form-initials__wrapper input');
+	var formTelField = document.querySelector('.form-tel__field');
+	var formEmailField = document.querySelector('.form-email__field');
+	var formExtraField = document.querySelector('.form-extra__field');
+	formExtraField.addEventListener('blur', function(evt) {
+		hideLabel(evt);
+	});
+	formTelField.addEventListener('blur', function(evt) {
+		hideLabel(evt);
+	});
+	formEmailField.addEventListener('blur', function(evt) {
+		hideLabel(evt);
+	});
+	for(var i = 0; i < formInitField.length; i++) {
+		formInitField[i].addEventListener('blur', function(evt) {
+			hideLabel(evt);
 		});
+	}
+	function hideLabel(evt) {
+		classNameHide = evt.target.className.split(' ')[0]+'-hide';
+		if(evt.target.value != '') {
+			if(!evt.target.classList.contains(classNameHide)) {
+				evt.target.classList.add(classNameHide);
+			}
+		} else {
+			if(evt.target.classList.contains(classNameHide)) {
+				evt.target.classList.remove(classNameHide);
+			}
+		}
 	}
 })();
